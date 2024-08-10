4501c7e130b52782b5b46fce8896203a4a205be0
QuantumVortexWhisper <acdorianjk@gmail.com>
2024-08-10 19:19:01 +0800

    Project1B Task2 Completed

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,10 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> wordDeque = new LinkedListDeque<Character>();
+        for (int i = 0; i < word.length(); i++) {
+            wordDeque.addLast(word.charAt(i));
+        }
+
+        return wordDeque;
+    }
+}
