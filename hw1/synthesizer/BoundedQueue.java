91a3340dc27273f3cd8551e8481ebc66b7ab3596
QuantumVortexWhisper <acdorianjk@gmail.com>
2024-08-13 21:16:02 +0800

    HW1 Task1 completed BoundedQueue created

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,31 @@
+package synthesizer;
+
+// items can only be enqueued at the back of the queue, and can only be dequeued from the front of the queue.
+// Unlike our Deque,the BoundedDeque has a fixed capacity, and nothing is allowed to enqueue if the queue is full.
+
+public interface BoundedQueue<T> {
+    // return size of the buffer
+    int capacity();
+
+    // return number of items currently in the buffer
+    int fillCount();
+
+    // add item x to the end
+    void enqueue(T x);
+
+    // delete and return item from the front
+    T dequeue();
+
+    // return (but do not delete) item from the front
+    T peek();
+
+    // is the buffer empty (fillCount equals zero)?
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    // is the buffer full (fillCount is same as capacity)?
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
+}
